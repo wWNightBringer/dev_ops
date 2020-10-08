b7cbf6988ce9ea453d5ee826679421fdd5bea377
artem.fedirchyk <artem@obscuredev.net>
2020-10-08 10:15:21 +0300

    UI initial commit

--- src/main/kotlin/com/project/dev_ops/DevOpsApplication.kt
+++ src/main/kotlin/com/project/dev_ops/DevOpsApplication.kt
@@ -1,6 +1,5 @@
 package com.project.dev_ops
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
