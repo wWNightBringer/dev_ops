be819d2de1fc7361234d07be7a74cb538fae350d
artem.fedirchyk <artem@obscuredev.net>
2020-10-15 10:33:29 +0300

    Created initial React components for getting common main route structure

---
+++ ui/src/components/HelloWorld.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from "../logo.svg";
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        <p>Here is caption: {this.props.caption}</p>
+                        <p>{this.props.text} =)</p>
+                    </p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld
---
+++ ui/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+
+function Navigation() {
+    return (
+        <nav>
+            <FontAwesomeIcon icon={faBars}/>
+        </nav>
+    )
+}
+
+export default Navigation
